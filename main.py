d1fdf129ddf442fa263cc7381502184971d67d49
ayushigarg2 <aggarwalayush251@gmail.com>
2023-07-10 23:30:02 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+Dict = {'https://www.amazon.in/Philips-109-3-inches-43PUT7791-Smart/dp/B01KCT2QNW/ref=lp_12516994031_1_1?sbo=RZvfv%2F%2FHxDF%2BO5021pAnSA%3D%3D': 25000,
+'https://www.amazon.in/Dell-Vostro-i5-1235U-35-56Cms-Windows/dp/B0BQJ7NMVM/ref=sr_1_14?keywords=dell+laptop&qid=1689010011&s=computers&sprefix=dell+%2Ccomputers%2C269&sr=1-14':50000,
+'https://www.amazon.in/Dell-Inspiron-i5-1335U-Comfortview-Thunderbolt/dp/B0C15CVNFG/ref=sr_1_2_sspa?keywords=dell+laptop&qid=1689010011&s=computers&sprefix=dell+%2Ccomputers%2C269&sr=1-2-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1':60000,
+'https://www.amazon.in/Samsung-Galaxy-Storage-Additional-Exchange/dp/B09CQ2TPDQ/ref=sr_1_13?keywords=samsung+mobile+phones&qid=1689009623&s=electronics&sr=1-13':45000,
+'https://www.amazon.in/Samsung-Galaxy-Prism-Silver-Storage/dp/B0C7C1DRK5/ref=sr_1_1_sspa?keywords=samsung+mobile+phones&qid=1689009623&s=electronics&sr=1-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1':15000,
+'https://www.amazon.in/dp/B0C5RY45C3?ie=UTF8&viewID=&ref_=s9_acss_bw_cg_INElec12_2b1_w':4000,
+}
+
